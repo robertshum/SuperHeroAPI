0303e7bcbb317c23a2a05428babf63a6e0ffa98e
robertshum <robertshum@gmail.com>
2024-04-17 01:37:14 -0700

    add migrations, sql connection, and ef core

--- SuperHeroAPI/Controllers/SuperHeroController.cs
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -7,37 +7,25 @@ namespace SuperHeroAPI.Controllers
     [ApiController]
     public class SuperHeroController : ControllerBase
     {
-        private static List<SuperHero> heroes = new List<SuperHero>
+        private readonly DataContext _context;
+
+        public SuperHeroController(DataContext context)
         {
-            new SuperHero {
-                Id = 1,
-                Name = "Spider Man!",
-                FirstName = "Peter",
-                LastName = "Parker",
-                Place = "New York City"
-            },
-            new SuperHero
-            {
-                Id = 2,
-                Name = "Iron Man",
-                FirstName = "Tony",
-                LastName = "Stark",
-                Place = "Long Island"
-            }
-        };
+            _context = context;
+        }
 
         [HttpGet]
         //Must return a specific type and not a interface
         public async Task<ActionResult<List<SuperHero>>> Get()
         {
-            return Ok(heroes);
+            return Ok(await _context.SuperHeros.ToListAsync());
         }
 
         [HttpGet("{id}")]
         //Must return a specific type and not a interface
         public async Task<ActionResult<SuperHero>> Get(int id)
         {
-            var hero = heroes.Find(h => h.Id == id);
+            var hero = await _context.SuperHeros.FindAsync(id);
             if (hero == null)
             {
                 return BadRequest("Hero/Villan not found.");
@@ -49,39 +37,48 @@ namespace SuperHeroAPI.Controllers
         //assumes this comes from the body, so we don't need the [FromBody] in front of SuperHero
         public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
         {
-            heroes.Add(hero);
-            return Ok(heroes);
+            _context.SuperHeros.Add(hero);
+
+            //we need to persist these changes
+            await _context.SaveChangesAsync();
+
+            //we need to re-fetch these changes
+            return Ok(await _context.SuperHeros.ToListAsync());
         }
 
         [HttpPut]
         public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero requestHero)
         {
-            var hero = heroes.Find(h => h.Id == requestHero.Id);
-            if (hero == null)
+            var dbHero = await _context.SuperHeros.FindAsync(requestHero.Id);
+            if (dbHero == null)
             {
                 return BadRequest("Hero/Villan not found.");
             }
 
-            hero.Name = requestHero.Name;
-            hero.FirstName = requestHero.FirstName;
-            hero.LastName = requestHero.LastName;
-            hero.Place = requestHero.Place;
+            dbHero.Name = requestHero.Name;
+            dbHero.FirstName = requestHero.FirstName;
+            dbHero.LastName = requestHero.LastName;
+            dbHero.Place = requestHero.Place;
 
-            return Ok(hero);
+            //persist changes
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.SuperHeros.ToListAsync());
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult<List<SuperHero>>> Delete(int id)
         {
-            var hero = heroes.Find(h => h.Id == id);
+            var hero = await _context.SuperHeros.FindAsync(id);
             if (hero == null)
             {
                 return BadRequest("Hero/Villan not found.");
             }
 
-            heroes.Remove(hero);
+            _context.SuperHeros.Remove(hero);
+            await _context.SaveChangesAsync();
 
-            return Ok(heroes);
+            return Ok(await _context.SuperHeros.ToListAsync());
         }
     }
 }
---
+++ SuperHeroAPI/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SuperHeroAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        //Table name SuperHeros
+        public DbSet<SuperHero> SuperHeros { get; set; }
+    }
+}
