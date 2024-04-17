53d985f576f88e0001d405bfe56dd58fb63c4487
robertshum <robertshum@gmail.com>
2024-04-16 20:57:19 -0700

    init framework w/ swagger

---
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private static List<SuperHero> heroes = new List<SuperHero>
+        {
+            new SuperHero {
+                Id = 1,
+                Name= "Spider Man!",
+                FirstName="Peter",
+                LastName="Parker",
+                Place="New York City"
+            }
+        };
+
+        [HttpGet]
+        //Must return a specific type and not a interface
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+            return Ok(heroes);
+        }
+    }
+}
